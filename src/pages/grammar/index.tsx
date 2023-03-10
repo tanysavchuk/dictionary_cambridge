dada5b07618084fad0089429039a593e1f86a1c1
tanysavchuk <tanysavchuk9@gmail.com>
2023-03-10 12:18:16 +0100

    criate component footer

--- src/pages/grammar/index.tsx
+++ src/pages/grammar/index.tsx
@@ -18,34 +18,60 @@ import { cardsContent } from "../../mocks/cards-content";
 import CardArticle from "../../components/cards-article";
 import WordOfTheDay from "../../components/card-word-ofthe-day";
 import { wordOfTheDay } from "../../mocks/card-word-of-the-day";
+import Footer from "../../components/footer";
+import {
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 const Grammar = () => {
   return (
     <>
-      <div className="flex justify-center bg-indigo-900 text-white p-5">
-        <div className=" flex items-center justify-between w-[90%]">
-          <div className="flex item-center">
-            <div className="">
-              <FontAwesomeIcon icon={faBars} />
+      <div className="flex justify-center bg-indigo-900 text-white px-5">
+        <div className=" flex items-center justify-between w-full ">
+          <div className="flex item-center h-full">
+            <div className="sm:border-r-0 md:border-r border-gray-400 pr-5 py-5">
+              <FontAwesomeIcon icon={faBars} className="text-2xl" />
             </div>
-            <div>
-              <Link to="#">Dictionary </Link>
+            <div className="md:flex items-center font-semibold sm:hidden">
+              <Link to="#" className="mx-5">
+                Dictionary
+              </Link>
               <Link to="#">Translate </Link>
-              <Link to="#">Grammar </Link>
+              <Link to="#" className="mx-5">
+                Grammar
+              </Link>
               <Link to="#">Thesaurus </Link>
-              <Link to="#">+Plus</Link>
+              <Link to="#" className="ml-5">
+                +Plus
+              </Link>
             </div>
           </div>
           <div className="flex item-center">
-            <div>
-              <Link to="#">
-                <FontAwesomeIcon icon={faUser} />
-                Log in / Sign up
+            <div className="lg:flex  items-end sm:hidden">
+              <Link to="https://www.facebook.com/CUPCambridgeDictionary/">
+                <FontAwesomeIcon icon={faFacebookF} />
+              </Link>
+              <Link
+                to="https://www.instagram.com/cambridgewords/"
+                className="mx-5"
+              >
+                <FontAwesomeIcon icon={faInstagram} />
+              </Link>
+              <Link to="https://twitter.com/CambridgeWords">
+                <FontAwesomeIcon icon={faTwitter} />
               </Link>
             </div>
-            <div>
-              <FontAwesomeIcon icon={faEarthAfrica} />
-              English (UK)
-              <FontAwesomeIcon icon={faAngleDown} />
+            <div className="mx-7 flex">
+              <Link to="#" className="flex items-center">
+                <FontAwesomeIcon icon={faUser} className="mr-2 text-sm" />
+                <p className="text-lg sm:hidden md:flex"> Log in / Sign up</p>
+              </Link>
+            </div>
+            <div className="text-light flex items-center">
+              <FontAwesomeIcon icon={faEarthAfrica} className="mr-2 text-sm" />
+              <p className="text-lg sm:hidden	lg:flex">English (UK)</p>
+              <FontAwesomeIcon icon={faAngleDown} className="ml-2 text-sm" />
             </div>
           </div>
         </div>
@@ -84,7 +110,7 @@ const Grammar = () => {
               width="254"
               height="80"
             ></img>
-            <p className="text-white mt-1 text-lg "> Grammar</p>
+            <p className="text-white mt-1 text-lg text-center"> Grammar</p>
           </div>
           <div className="w-[68%] my-16">
             <InputSearch />
@@ -135,9 +161,11 @@ const Grammar = () => {
       </div>
       <div>
         <div className="w-[75%]">
-          <div>
-            <h2>Explore the English Grammar</h2>
-            <p>
+          <div className="m-7 px-14 ">
+            <h2 className="mt-10 mb-5 text-3xl font-light">
+              Explore the English Grammar
+            </h2>
+            <p className="text-lg">
               Get clear grammar explanations with hundreds of examples of how
               grammar is used in natural written and spoken English.
             </p>
@@ -185,52 +213,77 @@ const Grammar = () => {
           </div>
           <div className="bg-cyan-100 w-full m-7 px-14 py-7">
             <div>
-              <h2>Popular searches</h2>
-              <ol className="grid grid-cols-2 gap-2.5 list-[decimal-leading-zero] list-inside	">
+              <h2 className="text-2xl font-light mb-4">Popular searches</h2>
+              <ol className="grid grid-cols-2 gap-2.5  list-inside font-semibold">
                 <Link to="#">
                   <li>
-                    Determiners (<span className="">the, my, some, this</span>)
+                    <span className="mr-1 font-light">01</span>
+                    Determiners (
+                    <span className="italic">the, my, some, this</span>)
                   </li>
                 </Link>
                 <Link to="#">
-                  <li>Table of irregular verbs</li>
-                </Link>
-                <Link to="#">
-                  <li>Word classes and phrase classes</li>
-                </Link>
-                <Link to="#">
-                  <li>Past continuous or past simple?</li>
-                </Link>
-                <Link to="#">
-                  <li>Dates</li>
+                  <li>
+                    <span className="mr-1 font-light">02</span>Table of
+                    irregular verbs
+                  </li>
                 </Link>
                 <Link to="#">
                   <li>
+                    <span className="mr-1 font-light">03</span>Word classes and
+                    phrase classes
+                  </li>
+                </Link>
+                <Link to="#">
+                  <li>
+                    {" "}
+                    <span className="mr-1 font-light">04</span>Past continuous
+                    or past simple?
+                  </li>
+                </Link>
+                <Link to="#">
+                  <li>
+                    {" "}
+                    <span className="mr-1 font-light">05</span>Dates
+                  </li>
+                </Link>
+                <Link to="#">
+                  <li>
+                    <span className="mr-1 font-light">06</span>
                     As, <span>when or while?</span>
                   </li>
                 </Link>
                 <Link to="#">
                   <li>
+                    <span className="mr-1 font-light">07</span>
                     <span>Since</span>
                   </li>
                 </Link>
                 <Link to="#">
-                  <li>Adverbs and adverb phrases: position</li>
+                  <li>
+                    {" "}
+                    <span className="mr-1 font-light">08</span>Adverbs and
+                    adverb phrases: position
+                  </li>
                 </Link>
                 <Link to="#">
                   <li>
+                    <span className="mr-1 font-light">09</span>
                     <span>Ought to</span>
                   </li>
                 </Link>
                 <Link to="#">
-                  <li>Inversion</li>
+                  <li>
+                    {" "}
+                    <span className="mr-1 font-light">10</span>Inversion
+                  </li>
                 </Link>
               </ol>
             </div>
           </div>
           <div className="bg-indigo-900 text-white m-7 px-14 py-7 w-full">
-            <h2>Browse the English Grammar</h2>
-            <div className="flex flex-wrap w-full">
+            <h2 className="text-3xl font-light">Browse the English Grammar</h2>
+            <div className="flex flex-wrap w-full my-5">
               {browseGrammars.map((browseGrammar, index) => (
                 <BrowseGrammar
                   key={`${browseGrammar.link} ${browseGrammar.name} ${index}`}
@@ -244,16 +297,19 @@ const Grammar = () => {
             </p>
           </div>
           <div className="m-7 px-14 w-full">
-            <h2>Key features</h2>
-            <p>
-              Adapted from <span>English Grammar Today</span>, the English
-              Grammar provides authentic examples of the way in which grammar is
-              used in real-life situations, including standard and non-standard
-              varieties of English. Written by a team of leading experts, and
-              based on extensive research on the Cambridge English Corpus, it’s
-              ideal for intermediate learners of English at CEFR levels B1-B2,
-              including anyone preparing for intermediate
-              <Link to="#">Cambridge Exams</Link> for schools.
+            <h2 className="text-3xl font-light mt-10 mb-5">Key features</h2>
+            <p className="mb-7 text-lg">
+              Adapted from <span className="italic">English Grammar Today</span>
+              , the English Grammar provides authentic examples of the way in
+              which grammar is used in real-life situations, including standard
+              and non-standard varieties of English. Written by a team of
+              leading experts, and based on extensive research on the Cambridge
+              English Corpus, it’s ideal for intermediate learners of English at
+              CEFR levels B1-B2, including anyone preparing for intermediate
+              <Link to="#" className="underline">
+                Cambridge Exams
+              </Link>
+              for schools.
             </p>
             <div className="flex flex-wrap border-b border-gray-300 pb-5">
               {cardsContent.map((cardContent, index) => (
@@ -263,16 +319,18 @@ const Grammar = () => {
                 />
               ))}
             </div>
-            <h3>Acknowledgements</h3>
-            <p>English Grammar Today: An A–Z of Spoken and Written Grammar</p>
-            <p>
-              <FontAwesomeIcon icon={faCopyright} />
+            <h3 className="mt-10 mb-5 text-3xl font-light">Acknowledgements</h3>
+            <p className="italic text-lg">
+              English Grammar Today: An A–Z of Spoken and Written Grammar
+            </p>
+            <p className="mb-10">
+              <FontAwesomeIcon icon={faCopyright} className="text-xs mr-1" />
               Cambridge University Press
             </p>
             <div className="flex  my-7">
               <div className="mr-12">
                 <h3 className="font-semibold text-lg">Authors</h3>
-                <ul className="list-none">
+                <ul className="list-none text-lg">
                   <li>Ronald Carter</li>
                   <li>Michael McCarthy</li>
                   <li>Geraldine Mark</li>
@@ -290,7 +348,7 @@ const Grammar = () => {
                 </ul>
               </div>
             </div>
-            <div>
+            <div className="mt-14">
               <CardArticle
                 picture="https://dictionary.cambridge.org/rss/images/back.jpg"
                 chapter="BLOG"
@@ -335,6 +393,11 @@ const Grammar = () => {
               />
             </div>
           </div>
+        </div>
+      </div>
+      <div className="bg-indigo-900 w-full flex justify-center p-5 mt-12">
+        <div className=" w-[95%]">
+          <Footer />
         </div>
       </div>
     </>
